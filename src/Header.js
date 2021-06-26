f66ca3e82eeb5ecab12d8145366274f86a37292e
alceil <ashishshajisrampickal@gmail.com>
2021-06-26 16:20:45 +0530

    styled header and added some css to it

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './Header.css'
 import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
 const Header = () => {
     return (
-        <div>
-            <PersonIcon/>
-            <h1>Hello kids!Lets crush it🚀💥</h1>
+        <div className="header">
+            <PersonIcon className="header__icon" fontSize ="large"/>
+      <img className="header__logo" src="https://media.designrush.com/inspirations/129349/conversions/_1513706350_604_tin-preview.jpg" alt="tinder"/>
+            <ForumIcon className="header__icon" fontSize ="large"/>
         </div>
     )
 }
