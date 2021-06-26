09483da518ff20b68fb80cd7010426221b988ead
alceil <ashishshajisrampickal@gmail.com>
2021-06-26 16:03:15 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-
+import Header from './Header'
 function App() {
   return (
     <div className="App">
   <h1>Hello borld</h1>
 
-
-  
+  <Header/>
+{/* {Header} */}
+{/* {Tinder Cards} */}
+{/* {Buttons below tinder cards} */} 
+{/* {Chatscreen} */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+const Header = () => {
+    return (
+        <div>
+            <PersonIcon/>
+            <h1>Hello kids!Lets crush it🚀💥</h1>
+        </div>
+    )
+}
+
+export default Header
